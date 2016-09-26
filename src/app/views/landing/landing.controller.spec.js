a6c7674e36a6d595596f88f57ee29d35aee2b2fa
Henley, Chanel (US - Arlington) <chenley@deloitte.com>
2016-09-26 12:46:35 -0400

    Created landing page, implemented download of NIEM model, and implemented overall styling for app

---
+++ src/app/views/landing/landing.controller.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe('Controller: LandingCtrl', function() {
+
+  // load the controller's module
+  beforeEach(module('dhsniem'));
+
+  var LandingCtrl;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function($injector) {
+    LandingCtrl = $injector.get('$controller')('LandingCtrl');
+  }));
+
+
+
+});
