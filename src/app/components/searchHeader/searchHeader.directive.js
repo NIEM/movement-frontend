6ee98d4cec7f05d841034d650f5a39783acd0bae
Henley, Chanel (US - Arlington) <chenley@deloitte.com>
2016-10-02 19:34:32 -0400

    Updated styling to home and results pages, based on latest comps

---
+++ src/app/components/searchHeader/searchHeader.directive.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ *
+ * @name searchHeader
+ *
+ * @description
+ * Header used on results and details pages
+ */
+(function() {
+
+  angular
+    .module('dhsniem')
+    .directive('searchHeader', searchHeader);
+
+  function searchHeader() {
+    return {
+      restrict: 'E',
+      templateUrl: 'app/components/searchHeader/searchHeader.directive.html',
+      scope: {},
+      link: link
+    };
+
+  }
+
+  /**
+   *  Defines variables and functions within niemExport scope
+   *
+   */
+  function link(scope) {
+
+  }
+
+})();
