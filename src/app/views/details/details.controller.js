14bc677bc70686effacb0ee20e3955b50ecdae2e
smrubin <sam.rubin@comcast.net>
2016-10-10 12:28:28 -0400

    added routing for the details page, made query for the entity on details page load

--- src/app/views/details/details.controller.js
+++ src/app/views/details/details.controller.js
@@ -14,7 +14,50 @@
     .module('dhsniem')
     .controller('DetailsCtrl', DetailsCtrl);
 
-  function DetailsCtrl() {
+  function DetailsCtrl(solrSearch, $location) {
+
+    var vm = this;
+
+
+    /**
+     * @name init
+     *
+     * @memberof dhsniem.controller:DetailsCtrl
+     *
+     * @description Initializes controller, retrieves data for the specific entity
+     */
+    function init() {
+
+      solrSearch.makeSolrRequest(buildSearchParams()).then(function(data) {
+        vm.entity = data.response.docs[0];
+      });
+
+    }
+
+
+    /**
+     * @name buildSearchParams
+     *
+     * @memberof dhsniem.controller:DetailsCtrl
+     *
+     * @description Buidls the search params to retrieve details data for the specific entity
+     */
+    function buildSearchParams() {
+
+      var id = $location.search().entityID;
+      var q = id.split(':')[0] + '\\:' + id.split(':')[1];
+
+      var params = {
+        'q': 'id:' + q,
+        'wt': 'json',
+        'json.wrf': 'JSON_CALLBACK',
+        'json.nl': 'map'
+      };
+
+      return params;
+    }
+
+    init();
 
   }
 
--- src/app/views/details/details.route.js
+++ src/app/views/details/details.route.js
@@ -6,8 +6,8 @@
     .module('dhsniem')
     .config(function($stateProvider) {
       $stateProvider
-        .state('main.', {
-          url: '/details',
+        .state('main.details', {
+          url: '/details?entityID',
           templateUrl: 'app/views/details/details.view.html',
           controller: 'DetailsCtrl',
           controllerAs: 'DetailsCtrl',
