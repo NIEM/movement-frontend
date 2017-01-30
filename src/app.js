eff730577151b5621368c4be185369caf7a7dc57
lizlee <leeliz742@gmail.com>
2017-01-30 11:32:07 -0500

    commenting out the sole url constant

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ angular
     'ngTouch',
     'ui.bootstrap'
   ])
-  .constant('SOLR_URL', 'http://localhost:8983/solr/dhsniem/select')
+  // .constant('SOLR_URL', 'http://localhost:8983/solr/dhsniem/select')
   .config(function($urlRouterProvider) {
     $urlRouterProvider.when('', '/'); // redirect to root if the state is ''
 
