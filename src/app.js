7b15c6f82428545dc5060e1b35552378a4f2d54a
srubin <srubin@deloitte.com>
2017-03-07 18:13:46 -0500

    added GA snippet to index, and having it load on run, and sending a pageview on state change

--- src/app.js
+++ src/app.js
@@ -17,13 +17,14 @@ angular
   ])
   .config(function ($urlRouterProvider) {
     $urlRouterProvider.when('', '/'); // redirect to root if the state is ''
-
     $urlRouterProvider.otherwise('/'); // redirect to root if state is not found
   })
 
-  .run(function ($rootScope, $window) {
+  .run(function ($rootScope, $window, $location) {
+    $window.ga('create', 'UA-27432921-1', 'auto');
     $rootScope.$on('$stateChangeSuccess', function () {
       $window.scroll(0, 0);
+      $window.ga('send', 'pageview', $location.url());
     });
   });
 
