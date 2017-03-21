7b03df00f25d1b15360daf3553dd89fd5678305c
srubin <srubin@deloitte.com>
2017-03-21 19:36:24 -0400

    added unit tests for solrDetails and niemChildAccordion

--- src/app/components/niemChildAccordion/niemChildAccordion.directive.spec.js
+++ src/app/components/niemChildAccordion/niemChildAccordion.directive.spec.js
@@ -6,7 +6,7 @@ describe('directive:niemChildAccordion', function () {
   beforeEach(module('dhsniem'));
   beforeEach(module('templates'));
 
-  var element, scope, elScope, $compile, $httpBackend;
+  var element, scope, elScope, $compile, $httpBackend, niemTree;
 
   // Initialize a mock scope
   beforeEach(inject(function ($injector) {
@@ -14,24 +14,29 @@ describe('directive:niemChildAccordion', function () {
     scope = $injector.get('$rootScope').$new();
     $compile = $injector.get('$compile');
     $httpBackend = $injector.get('$httpBackend');
+    niemTree = $injector.get('niemTree');
 
     $httpBackend.whenGET(new RegExp('\\' + 'uib/template')).respond(200, {});
 
     element = angular.element('<niem-child-accordion element-data="elementData"></niem-child-accordion>');
-    scope.elementData = {type: 'CardType', elements: ["nc:ObligationCategoryText"]};
+    scope.elementData = {type: {elements: ['nc:CardSubElement']}};
     element = $compile(element)(scope);
     scope.$apply();
     elScope = element.isolateScope();
 
   }));
 
-  // it('should open and handle click on click', function () {
-  //   elScope.isOpen = false;
-  //   elScope.expandElement();
-  //   expect(elScope.isOpen).toBe(true);
-  //   expect(elScope.dataFound).toBe(true);
-  //   elScope.expandElement();
-  //   expect(elScope.dataFound).toBe(true);
-  // });
+  it('should open and handle click on click', function () {
+    spyOn(niemTree, 'getElementObjects').and.callThrough();
+    elScope.isOpen = false;
+    elScope.dataFound = false;
+    elScope.expandElement();
+    expect(elScope.isOpen).toBe(true);
+    expect(niemTree.getElementObjects).toHaveBeenCalled();
+    expect(elScope.dataFound).toBe(true);
+    elScope.expandElement();
+    expect(elScope.dataFound).toBe(true);
+    elScope.expandElement();
+  });
 
 });
--- src/app/components/solrDetails/solrDetails.directive.spec.js
+++ src/app/components/solrDetails/solrDetails.directive.spec.js
@@ -6,31 +6,31 @@ describe('directive:solrDetails', function() {
   beforeEach(module('dhsniem'));
   beforeEach(module('templates'));
 
-  var element, scope, elScope, $compile, solrRequest, $location, $window;
+  var element, scope, elScope, $compile, solrRequest, $location, $window, $httpBackend, SOLR_URL;
 
   // Initialize a mock scope
   beforeEach(inject(function ($injector) {
     scope = $injector.get('$rootScope').$new();
     $compile = $injector.get('$compile');
     $location = $injector.get('$location');
+    $httpBackend = $injector.get('$httpBackend');
     $window = $injector.get('$window');
+    SOLR_URL = $injector.get('SOLR_URL');
     solrRequest = $injector.get('solrRequest');
     $location.search('entityID', 'Core:Alert');
 
-    element = angular.element('<solr-my-schema-details></solr-my-schema-details>');
+    $httpBackend.whenJSONP(new RegExp('\\' + SOLR_URL)).respond(200, {});
+
+    element = angular.element('<solr-details></solr-details>');
     element = $compile(element)(scope);
     scope.$apply();
     elScope = element.scope();
   }));
 
-  // it('should transform namespace text', function () {
-  //   expect(elScope.formatNamespaceType('domain')).toBe('Domain');
-  // });
-
-  // it('should go back to search results', function () {
-  //   spyOn($window.history, 'back');
-  //   elScope.goBack();
-  //   expect($window.history.back).toHaveBeenCalled();
-  // });
+  it('should go back to search results', function () {
+    spyOn($window.history, 'back');
+    elScope.goBack();
+    expect($window.history.back).toHaveBeenCalled();
+  });
 
 });
