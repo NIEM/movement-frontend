6d1566556ec2eaff04303ba74e2037f063460f30
srubin <srubin@deloitte.com>
2017-03-27 12:21:08 -0400

    added unit tests for schemaAddRemoveFromSchemaButton

--- src/app/components/schemaAddRemoveFromSchemaButton/schemaAddRemoveFromSchemaButton.directive.spec.js
+++ src/app/components/schemaAddRemoveFromSchemaButton/schemaAddRemoveFromSchemaButton.directive.spec.js
@@ -6,18 +6,32 @@ describe('directive:schemaAddRemoveFromSchemaButton', function () {
   beforeEach(angular.mock.module('dhsniem'));
   beforeEach(angular.mock.module('templates'));
 
-  var element, scope;
+  var element, scope, $compile, elScope, mySchema;
 
   // Initialize a mock scope
   beforeEach(inject(function ($injector) {
     scope = $injector.get('$rootScope').$new();
-  }));
+    $compile = $injector.get('$compile');
+    mySchema = $injector.get('mySchema');
 
-  // compile the element to be tested
-  it('should be a thing', inject(function ($compile) {
     element = angular.element('<schema-add-remove-from-schema-button></schema-add-remove-from-schema-button>');
     element = $compile(element)(scope);
-
     scope.$apply();
+    elScope = element.isolateScope();
+  }));
+
+  it('should toggle between add and remoe', inject(function () {
+    spyOn(mySchema, 'removeFromSchema');
+    spyOn(mySchema, 'addToSchema');
+    // expect(mySchema.removeFromSchema).toHaveBeenCalled();
+    elScope.isInMySchema = false;
+    elScope.toggleSchemaAddRemove();
+    expect(elScope.isInMySchema).toEqual(true);
+    expect(elScope.schemaAddRemoveButton).toEqual('Remove from Schema');
+    expect(mySchema.addToSchema).toHaveBeenCalled();
+    elScope.toggleSchemaAddRemove();
+    expect(elScope.isInMySchema).toEqual(false);
+    expect(elScope.schemaAddRemoveButton).toEqual('Add to Schema');
+    expect(mySchema.removeFromSchema).toHaveBeenCalled();
   }));
 });
