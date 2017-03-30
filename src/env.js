0bc97dec4aae83cd9bed14416b67cf53ef5566ff
lizlee <leeliz742@gmail.com>
2017-03-30 15:09:27 -0400

    reverting back the env.js constants for local dev and editing git ignore file

--- src/env.js
+++ src/env.js
@@ -1,8 +1,9 @@
 "use strict";
 
-angular.module('dhsniem')
+ angular.module('dhsniem')
 
-  .constant('SOLR_URL', 'http://35.164.75.93:8983/solr/dhsniem/select')
+.constant('SOLR_URL', 'http://35.164.75.93:8983/solr/dhsniem/select')
 
-  .constant('NODE_URL', 'http://35.164.75.93:7000/jsonschema?')
+.constant('NODE_URL', 'http://35.164.75.93:7000/jsonschema?')
+
 ;
