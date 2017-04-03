8683306d93aa6a59a83a8ecafedf2ca1937899f5
srubin <srubin@deloitte.com>
2017-04-03 16:27:22 -0400

    added service worker shell

--- src/app.js
+++ src/app.js
@@ -23,5 +23,14 @@ angular
       $window.scroll(0, 0);
       $window.ga('send', 'pageview', $location.url());
     });
+
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch(function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    }
+
   });
 
