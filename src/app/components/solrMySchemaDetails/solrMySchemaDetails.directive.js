6344e6e6453ddd645991132a354a533a3dd049af
srubin <srubin@deloitte.com>
2017-04-03 21:24:07 -0400

    updated check of myschema ids to use length to fix console error

--- src/app/components/solrMySchemaDetails/solrMySchemaDetails.directive.js
+++ src/app/components/solrMySchemaDetails/solrMySchemaDetails.directive.js
@@ -82,7 +82,7 @@
        */
       function getSchema() {
         scope.mySchemaIDs = mySchema.getSchema();
-        if (scope.mySchemaIDs) {
+        if (scope.mySchemaIDs.length) {
           getSchemaArray();
         }
       }
