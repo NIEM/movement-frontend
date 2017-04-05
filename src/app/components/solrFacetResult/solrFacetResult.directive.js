d3e61a0232214325ceef7c6e74e37927a469db86
lizlee <leeliz742@gmail.com>
2017-04-05 10:07:14 -0400

    reverting back testing changes

--- src/app/components/solrFacetResult/solrFacetResult.directive.js
+++ src/app/components/solrFacetResult/solrFacetResult.directive.js
@@ -74,7 +74,6 @@
         } else {
           selectedFacets.splice(selectedFacets.indexOf(scope.facetString()), 1);
         }
-        console.log(selectedFacets); 
         $location.search('selectedFacets', selectedFacets);
         $location.search('page', 1);
         solrSearch.search();
